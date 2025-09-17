c380253c70bde18fbf2bf46cabc230afca379d10
Asha Luckins <ashaluckinsanoop@gmail.com>
2025-09-17 12:38:28 -0400

    add State to switch between the noProjectsAdded screen and newProject Screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import ProjectsSidebar from "./components/ProjectsSideBar.jsx";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectselected.jsx";
+import { useState } from "react";
 function App() {
+  const [projectsState, setProjectsState]=useState({
+    selectedProjectId:undefined,// we are using undefined for no projects available, null for adding new project button clicked and id when project is created
+    projects:[],
+  });
+  let content;
+  function handleNewProjectClick(){
+    setProjectsState(prevState=>{
+      return{
+        ...prevState,
+        selectedProjectId:null,
+      }
+  })
+  }
+  if(projectsState.selectedProjectId===null){
+    content=<NewProject/>
+  }else if(projectsState.selectedProjectId===undefined){
+    content=<NoProjectSelected onClickNewProject={handleNewProjectClick}/>
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar/>
-      <NoProjectSelected/>
-      {/* <NewProject/> */}
+      <ProjectsSidebar onClickNewProject={handleNewProjectClick}/>      
+      {content}
     </main>
   );
 }
--- src/components/ProjectsSideBar.jsx
+++ src/components/ProjectsSideBar.jsx
@@ -1,11 +1,11 @@
 import Button from "./Button.jsx"
-export default function ProjectsSidebar(){
+export default function ProjectsSidebar({onClickNewProject}){
     return(
         <aside className="w-1/3 px-6 py-12 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
             <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-50">Your Projects</h2>
             <ul></ul>
             <div>
-                <Button>+ Add Project</Button>
+                <Button onClick={onClickNewProject}>+ Add Project</Button>
             </div>            
         </aside>
     )
